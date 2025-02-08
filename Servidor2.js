1168fe05b8738857966e4c0aa380e085d12bc260
Giuliano Paolo Sacco <gsacco009@gmail.com>
2025-02-08 15:52:12 -0300

    Add files via upload

---
+++ Servidor2.js
@@ -0,0 +1,17 @@
+const http = require(`http`);
+
+const servidor = http.createServer((req, res) => {
+    console.log(`===> res (respuesta)`);
+    
+    res.setHeader(`content.type`, `application/json`);
+    console.log(res.getHeaders());
+    
+    res.end(`Hola Mundo`);
+});
+
+const puerto = 3000;
+
+servidor.listen(puerto, ()=> {
+    console.log(`El servidor esta escuchando en el puerto ${puerto}...`);
+
+});
